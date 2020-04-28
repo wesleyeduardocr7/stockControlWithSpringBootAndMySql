88a1c3962a15f504885cde435e9e527372e6e738
wesleyeduardocr7 <wesley.eduardo@acad.ifma.edu.br>
2020-04-28 08:39:21 -0300

    initialcommit

---
+++ src/test/java/br/com/wesleyeduardo/stockcontrol/StockcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wesleyeduardo.stockcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
